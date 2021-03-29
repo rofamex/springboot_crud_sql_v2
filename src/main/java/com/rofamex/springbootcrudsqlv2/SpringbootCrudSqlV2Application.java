4db518b7d0edee943a1ea74778e7927ccc1777ba
Robson Lee <rofamex@gmail.com>
2021-03-29 17:38:05 -0400

    adding libraries

---
+++ src/main/java/com/rofamex/springbootcrudsqlv2/SpringbootCrudSqlV2Application.java
@@ -0,0 +1,13 @@
+package com.rofamex.springbootcrudsqlv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudSqlV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudSqlV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rofamex/springbootcrudsqlv2/SpringbootCrudSqlV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rofamex.springbootcrudsqlv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudSqlV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
